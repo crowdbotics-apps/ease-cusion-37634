74ae38b4a5f8b0f1bf607dd38e3752ce58dce615
Crowdbotics <team@crowdbotics.com>
2022-11-18 15:53:11 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/ease_cusion_37634/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ease_cusion_37634;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "ease_cusion_37634";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "ease_cusion_37634",
+  url: "https://ease_cusion_37634.botics.co",
+  api: "https://ease_cusion_37634.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
